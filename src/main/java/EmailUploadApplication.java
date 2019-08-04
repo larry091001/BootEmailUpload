3335b8de08c30005dd0e8f16f6f9e6079a464507
zhang_ying <zhang_ying@suixingpay.com>
2019-08-04 16:39:02 +0800

    Initial commit

---
+++ src/main/java/EmailUploadApplication.java
@@ -0,0 +1,14 @@
+/**
+ * @version V1.0
+ * @Author: Larry(PC)
+ * @Email: zhang_ying@suixingpay.com
+ * @phone: 13552892515
+ * 创建日期：2019/8/4 15:55
+ */
+@SpringBootApplication
+public class EmailUploadApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(FileUploadApplication.class, args);
+        System.out.println("FileUploadApplication 程序启动成功!");
+    }
+}
---
+++ src/main/java/com/larry/boot/dojo/Mail.java
@@ -0,0 +1,55 @@
+package com.larry.boot.dojo;
+
+
+/**
+ * @version V1.0
+ * @Author: Larry(PC)
+ * @Email: zhang_ying@suixingpay.com
+ * @phone: 13552892515
+ * 创建日期：2019/8/4 16:08
+ */
+public class Mail {
+    /** 发送者*/
+    private String sender;
+
+    /** 接受者 */
+    private String receiver;
+
+    /** 主题 */
+    private String subject;
+
+    /** 发送 消息*/
+    private String text;
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(String receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+}
